5dc05737f1ed08a1da1c1ba757a1ec685eefdb4f
Sergey <m.sekohan@gmail.com>
2018-05-05 19:22:35 +0300

    Done

---
+++ SmtpClient/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SmtpClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxReceiver = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBoxPassword = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxTopic = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(100, 60);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(292, 22);
+            this.textBoxLogin.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Логин:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пароль:";
+            // 
+            // textBoxReceiver
+            // 
+            this.textBoxReceiver.Location = new System.Drawing.Point(100, 155);
+            this.textBoxReceiver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxReceiver.Name = "textBoxReceiver";
+            this.textBoxReceiver.Size = new System.Drawing.Size(292, 22);
+            this.textBoxReceiver.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 159);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Адресат:";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(100, 251);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(292, 106);
+            this.textBoxMessage.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 255);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Сообщение:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 373);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBoxPassword
+            // 
+            this.maskedTextBoxPassword.Location = new System.Drawing.Point(100, 106);
+            this.maskedTextBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.maskedTextBoxPassword.Name = "maskedTextBoxPassword";
+            this.maskedTextBoxPassword.PasswordChar = '*';
+            this.maskedTextBoxPassword.Size = new System.Drawing.Size(292, 22);
+            this.maskedTextBoxPassword.TabIndex = 11;
+            // 
+            // textBoxTopic
+            // 
+            this.textBoxTopic.Location = new System.Drawing.Point(100, 203);
+            this.textBoxTopic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxTopic.Name = "textBoxTopic";
+            this.textBoxTopic.Size = new System.Drawing.Size(292, 22);
+            this.textBoxTopic.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 207);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Тема:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 417);
+            this.Controls.Add(this.textBoxTopic);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.maskedTextBoxPassword);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxReceiver);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SmtpClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxReceiver;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPassword;
+        private System.Windows.Forms.TextBox textBoxTopic;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SmtpClient/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace SmtpClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                MailAddress from = new MailAddress(textBoxLogin.Text,"SM TP");
+                // кому отправляем
+                MailAddress to = new MailAddress(textBoxReceiver.Text);
+                // создаем объект сообщения
+                MailMessage m = new MailMessage(from, to);
+
+                // тема письма
+                m.Subject = textBoxTopic.Text;
+                // текст письма
+                m.Body = textBoxMessage.Text;
+
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+                client.Credentials = new NetworkCredential(textBoxLogin.Text, maskedTextBoxPassword.Text);
+                client.EnableSsl = true;
+
+                try
+                {
+                    client.Send(m);
+                }
+                catch (SmtpException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод данных!");
+            }
+        }
+    }
+}
